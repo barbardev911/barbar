a4b00028dfc277207bd97b683519707eeef66f21
Hadi Lazuardi <hadilazuardii@gmail.com>
2019-11-06 13:52:35 +0700

    Barbar landing page

--- index.php
+++ index.php
@@ -1,12 +1,116 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>BarbarDev</title>
+    <link rel="icon" href="https://www.gstatic.com/images/branding/product/ico/google_fonts_lodp.ico" type="image/x-icon">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Baloo+Bhai|Kaushan+Script&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/custom.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </head>
+
 <body>
-    BARBAR PHP
+    <div class="mynavbar mygradient">
+        <div class="col-sm-12 row">
+            <div class="col-sm-3">
+                <h2 class="mylogo">BarbarDev</h2>
+            </div>
+            <div class="mynavmenu right mygradient">
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Tutorials</a>
+                <a href="#">QA</a>
+                <a href="#">Videos</a>
+                <a href="#">Contact</a>
+            </div>
+        </div>
+    </div>
+    <br>
+    <main>
+        <section>
+            <div class="col-sm-12 row">
+                <div class="col-sm-9">
+
+                </div>
+                <div class="col-sm-3">
+                    SEARCH HERE <br> <br>
+                </div>
+            </div>
+            <div class="col-sm-12 row">
+                <div class="col-sm-9 mycardbox mygradient">
+                    <label class="mylabel">Latest Article</label>
+                    <div class="col-sm-12 row mygradient">
+                        <div class="col-sm-3 mycardbox mypostcard">
+                            <img src="https://via.placeholder.com/200x200?text=Visit+Blogging.com+Now" alt="" width="100%">
+                        </div>
+                        <div class="col-sm-9 mycardbox mypostcard">
+                            <label class="mylabel">Latest Article</label>
+                            <p>
+                                Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
+                            </p>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="col-sm-12 row mygradient">
+                        <div class="col-sm-3 mycardbox mypostcard">
+                            <img src="https://via.placeholder.com/200x200?text=Visit+Blogging.com+Now" alt="" width="100%">
+                        </div>
+                        <div class="col-sm-9 mycardbox mypostcard">
+                            <label class="mylabel">Latest Article</label>
+                            <p>
+                                Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
+                            </p>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="col-sm-12 row mygradient">
+                        <div class="col-sm-3 mycardbox mypostcard">
+                            <img src="https://via.placeholder.com/200x200?text=Visit+Blogging.com+Now" alt="" width="100%" height="100%">
+                        </div>
+                        <div class="col-sm-9 mycardbox mypostcard">
+                            <label class="mylabel">Latest Article</label>
+                            <p>
+                                Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
+                            </p>
+                        </div>
+                    </div>
+                    <br>
+                </div>
+                <div class="col-sm-3 mycardbox mygradient">
+                    <label class="mylabel">Latest Tutorial</label>
+                    <div class="col-sm-12 mycardbox mygradient">
+                        Tutorial 1
+                    </div>
+                    <br>
+                    <div class="col-sm-12 mycardbox mygradient">
+                        Tutorial 2
+                    </div>
+                    <br>
+                    <div class="col-sm-12 mycardbox mygradient">
+                        Tutorial 3
+                    </div>
+                    <br>
+                    <div class="col-sm-12 mycardbox mygradient">
+                        Tutorial 4
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <br><br>
+    <div class="mynavbar mygradient">
+        <div class="col-sm-12 row">
+            <div class="col-sm-3">
+                <h2 class="mylogo">@BarbarDev</h2>
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
